35cc36b020d7b103c41f75706105fbdc9d20d3a4
Subash Mylraj <subashmylraj@gmail.com>
2020-10-22 18:24:32 +0300

    7 working

--- Lab-5/src/7.c
+++ Lab-5/src/7.c
@@ -2,46 +2,81 @@
 #include<sys/types.h>
 #include<unistd.h>
 #include<stdlib.h>
+#include<pthread.h>
 
-void binary_search (int [], int, int, int);
+struct arguments{
+	int arr[20];
+	int start;
+	int end;
+	int key;
+};
+
+pthread_t threads[100];
+int itr=0;
+
+void* binary_search (void *);
 
 int main (){
 
-	int arr[] = {3, 1, 5, 2, 5, 2, 6, 4, 7, 9, 5, 3, 5}, n = sizeof(arr)/sizeof(int);
-	int key = 5;
-	binary_search(arr, 0, n, key);
+	struct arguments *args = (struct arguments *)malloc(sizeof(struct arguments));
+	int n;
+	printf("Enter size of array: ");
+	scanf("%d", &n);
+
+	printf("Enter elements of array: ");
+	for(int i=0; i<n; i++)
+		scanf("%d", &args->arr[i]);
+
+	printf("Enter key to search for: ");
+	scanf("%d", &args->key);
+
+	args->end = n;
+	args->start = 0;
+
+	binary_search(args);
+
+	for(int i=0; i<itr; i++){
+		pthread_join(threads[i], NULL);
+	}
 
 	return 0;
 
 }
 
-void binary_search (int arr[], int start, int end, int key){
+void* binary_search (void* a){
 
-	int mid = (end + start)/2;
+	struct arguments* args = (struct arguments*) a;
+	int mid = (args->end + args->start)/2;
+	int end = args->end, start = args->start;
 
-	if(start > end){
-		return;
+	if(args->start > args->end){
+		return NULL;
 	}
-	if(start == end){
-		if(arr[mid] == key){
+	if(args->start == args->end){
+		if(args->arr[mid] == args->key){
 			printf("Key found at address: %d\n", mid);
 		}
-		return;
+		return NULL;
 	}
-	if(start+1 == end){
-		binary_search(arr, start, start, key);
-		binary_search(arr, end, end, key);
+	if(args->start+1 == args->end){
+
+			args->end=args->start;
+			pthread_create(&threads[itr], NULL, binary_search, args);itr++;
+
+			args->start=args->end=end;
+			pthread_create(&threads[itr], NULL, binary_search, args);itr++;
+			args->start=start; args->end=end;
 	}
-	if(arr[mid] == key){
+	if(args->arr[mid] == args->key){
 		printf("Key found at address: %d\n", mid);
 	}
 	
-	pid_t pid = fork();
-	if(pid == 0){
-		binary_search(arr, start, mid-1, key);
-	}
-	else{
-		binary_search(arr, mid+1, end, key);
-	}
-
+		args->end = mid-1;
+		binary_search(args);
+		pthread_create(&threads[itr], NULL, binary_search, args);itr++;
+	
+		args->start = mid+1;
+		args->end = end;
+		binary_search(args);
+		pthread_create(&threads[itr], NULL, binary_search, args);itr++;
 }
